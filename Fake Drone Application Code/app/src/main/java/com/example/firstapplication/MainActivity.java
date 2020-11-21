996e0383a3856c06a13c28bf22afa0b8da6d8268
Bochan kim <44803830+bo1030@users.noreply.github.com>
2020-11-21 19:02:11 +0900

    add parser and communication part

--- Fake Drone Application Code/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ Fake Drone Application Code/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -4,12 +4,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button startBtn, stopBtn;
     TextView battery, positionPitch, positionRoll, positionYaw , vibrationX, vibrationY, vibrationZ, controlPitch, controlRoll, controlYaw, locationX, locationY, locationZ, mod, ekfStatus, task;
 
+    private static CSEBase csebase = new CSEBase();
+    private static AE ae = new AE();
+    private static String TAG = "MainActivity";
+    private String MQTTPort = "1883";
+    private String ServiceAEName = "CDP5";
+    private String MQTT_Req_Topic = "";
+    private String MQTT_Resp_Topic = "";
+    private MqttAndroidClient mqttClient = null;
+    private String Mobius_Address ="13.209.165.214";
+    private Timer timer;
+    private TimerTask download;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
         startBtn = (Button) findViewById(R.id.startButton);
         stopBtn = (Button) findViewById(R.id.stopButton);
+        startBtn.setOnClickListener(this);
+        stopBtn.setOnClickListener(this);
 
         battery = (TextView) findViewById(R.id.batteryInfo);
 
@@ -41,7 +78,283 @@ public class MainActivity extends AppCompatActivity {
 
         ekfStatus = (TextView) findViewById(R.id.efkInfo);
         task = (TextView) findViewById(R.id.missionInfo);
+    }
 
+    public void GetAEInfo() {
+        csebase.setInfo(Mobius_Address,"7579","Mobius","1883");
 
+        //csebase.setInfo("203.253.128.151","7579","Mobius","1883");
+        // AE Create for Android AE
+        ae.setAppName("ncubeapp");
+        aeCreateRequest aeCreate = new aeCreateRequest();
+        aeCreate.setReceiver(new IReceived() {
+            public void getResponseBody(final String msg) {
+                handler.post(new Runnable() {
+                    public void run() {
+                        Log.d(TAG, "** AE Create ResponseCode[" + msg +"]");
+                        if( Integer.parseInt(msg) == 201 ){
+                            MQTT_Req_Topic = "/oneM2M/req/Mobius2/"+ae.getAEid()+"_sub"+"/#";
+                            MQTT_Resp_Topic = "/oneM2M/resp/Mobius2/"+ae.getAEid()+"_sub"+"/json";
+                            Log.d(TAG, "ReqTopic["+ MQTT_Req_Topic+"]");
+                            Log.d(TAG, "ResTopic["+ MQTT_Resp_Topic+"]");
+                        }
+                        else { // If AE is Exist , GET AEID
+                            aeRetrieveRequest aeRetrive = new aeRetrieveRequest();
+                            aeRetrive.setReceiver(new IReceived() {
+                                public void getResponseBody(final String resmsg) {
+                                    handler.post(new Runnable() {
+                                        public void run() {
+                                            Log.d(TAG, "** AE Retrive ResponseCode[" + resmsg +"]");
+                                            MQTT_Req_Topic = "/oneM2M/req/Mobius2/"+ae.getAEid()+"_sub"+"/#";
+                                            MQTT_Resp_Topic = "/oneM2M/resp/Mobius2/"+ae.getAEid()+"_sub"+"/json";
+                                            Log.d(TAG, "ReqTopic["+ MQTT_Req_Topic+"]");
+                                            Log.d(TAG, "ResTopic["+ MQTT_Resp_Topic+"]");
+                                        }
+                                    });
+                                }
+                            });
+                            aeRetrive.start();
+                        }
+                    }
+                });
+            }
+        });
+        aeCreate.start();
+    }
+
+    private List<String> parsingf(String xml)
+    {
+        List<String> data = new ArrayList<String>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentbuilder = factory.newDocumentBuilder(); 
+            InputStream is = new ByteArrayInputStream(xml.getBytes());
+            Document doc = documentbuilder.parse(is);
+            Element element = doc.getDocumentElement();
+            NodeList list = element.getElementsByTagName("con");
+            for (int i = 0; i < list.getLength(); i++) {
+                Node node = list.item(i);
+                Node temp = node.getFirstChild();
+                String value = temp.getNodeValue();
+                data.add(value);
+            }
+ 
+        } catch (Exception e) {
+            System.out.println("parsing error" + e.getMessage());
+        }
+
+        return data;
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.startBtn:{
+                RetrieveRequest bat = new RetrieveRequest("battery");
+                RetrieveRequest con = new RetrieveRequest("control");
+                RetrieveRequest loc = new RetrieveRequest("location");
+                RetrieveRequest tas = new RetrieveRequest("task");
+                RetrieveRequest EKF = new RetrieveRequest("EKF");
+                RetrieveRequest vib = new RetrieveRequest("vibration");
+                RetrieveRequest pos = new RetrieveRequest("position");
+                RetrieveRequest mode = new RetrieveRequest("flightmode");
+                
+                break;  
+            }
+            case R.id.stopBtn:{
+                break;
+            }
+        }
+    }
+
+     @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+
+    public interface IReceived {
+        void getResponseBody(String msg);
+    }
+
+    class RetrieveRequest extends Thread {
+        private final Logger LOG = Logger.getLogger(RetrieveRequest.class.getName());
+        private IReceived receiver;
+        private String ContainerName;
+
+        public RetrieveRequest(String containerName) {
+            this.ContainerName = containerName;
+        }
+        public RetrieveRequest() {}
+        public void setReceiver(IReceived hanlder) { this.receiver = hanlder; }
+
+        @Override
+        public void run() {
+            try {
+                String sb = csebase.getServiceUrl() + "/" + ServiceAEName + "/" + ContainerName + "/" + "latest";
+
+                URL mUrl = new URL(sb);
+
+                HttpURLConnection conn = (HttpURLConnection) mUrl.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(false);
+
+                conn.setRequestProperty("Accept", "application/xml");
+                conn.setRequestProperty("X-M2M-RI", "12345");
+                conn.setRequestProperty("X-M2M-Origin", ae.getAEid() );
+                conn.setRequestProperty("nmtype", "long");
+                conn.connect();
+
+                String strResp = "";
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String strLine= "";
+                while ((strLine = in.readLine()) != null) {
+                    strResp += strLine;
+                }
+
+                if ( strResp != "" ) {
+                    receiver.getResponseBody(strResp);
+                }
+                conn.disconnect();
+
+            } catch (Exception exp) {
+                LOG.log(Level.WARNING, exp.getMessage());
+            }
+        }
+    }
+
+     class aeCreateRequest extends Thread {
+        private final Logger LOG = Logger.getLogger(aeCreateRequest.class.getName());
+        String TAG = aeCreateRequest.class.getName();
+        private IReceived receiver;
+        int responseCode=0;
+        public ApplicationEntityObject applicationEntity;
+        public void setReceiver(IReceived hanlder) { this.receiver = hanlder; }
+        public aeCreateRequest(){
+            applicationEntity = new ApplicationEntityObject();
+            applicationEntity.setResourceName(ae.getappName());
+        }
+        @Override
+        public void run() {
+            try {
+
+                String sb = csebase.getServiceUrl();
+                URL mUrl = new URL(sb);
+
+                HttpURLConnection conn = (HttpURLConnection) mUrl.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setInstanceFollowRedirects(false);
+
+                conn.setRequestProperty("Content-Type", "application/vnd.onem2m-res+xml;ty=2");
+                conn.setRequestProperty("Accept", "application/xml");
+                conn.setRequestProperty("locale", "ko");
+                conn.setRequestProperty("X-M2M-Origin", "S"+ae.getappName());
+                conn.setRequestProperty("X-M2M-RI", "12345");
+                conn.setRequestProperty("X-M2M-NM", ae.getappName() );
+
+                String reqXml = applicationEntity.makeXML();
+                conn.setRequestProperty("Content-Length", String.valueOf(reqXml.length()));
+
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+                dos.write(reqXml.getBytes());
+                dos.flush();
+                dos.close();
+
+                responseCode = conn.getResponseCode();
+
+                BufferedReader in = null;
+                String aei = "";
+                if (responseCode == 201) {
+                    // Get AEID from Response Data
+                    in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    String resp = "";
+                    String strLine;
+                    while ((strLine = in.readLine()) != null) {
+                        resp += strLine;
+                    }
+
+                    ParseElementXml pxml = new ParseElementXml();
+                    aei = pxml.GetElementXml(resp, "aei");
+                    ae.setAEid( aei );
+                    Log.d(TAG, "Create Get AEID[" + aei + "]");
+                    in.close();
+                }
+                if (responseCode != 0) {
+                    receiver.getResponseBody( Integer.toString(responseCode) );
+                }
+                conn.disconnect();
+            } catch (Exception exp) {
+                LOG.log(Level.SEVERE, exp.getMessage());
+            }
+
+        }
+    }
+
+    class aeRetrieveRequest extends Thread {
+        private final Logger LOG = Logger.getLogger(aeCreateRequest.class.getName());
+        private IReceived receiver;
+        int responseCode=0;
+
+        public aeRetrieveRequest() {
+        }
+        public void setReceiver(IReceived hanlder) {
+            this.receiver = hanlder;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String sb = csebase.getServiceUrl()+"/"+ ae.getappName();
+                URL mUrl = new URL(sb);
+
+                HttpURLConnection conn = (HttpURLConnection) mUrl.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(false);
+
+                conn.setRequestProperty("Accept", "application/xml");
+                conn.setRequestProperty("X-M2M-RI", "12345");
+                conn.setRequestProperty("X-M2M-Origin", "Sandoroid");
+                conn.setRequestProperty("nmtype", "short");
+                conn.connect();
+
+                responseCode = conn.getResponseCode();
+
+                BufferedReader in = null;
+                String aei = "";
+                if (responseCode == 200) {
+                    // Get AEID from Response Data
+                    in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    String resp = "";
+                    String strLine;
+                    while ((strLine = in.readLine()) != null) {
+                        resp += strLine;
+                    }
+
+                    ParseElementXml pxml = new ParseElementXml();
+                    aei = pxml.GetElementXml(resp, "aei");
+                    ae.setAEid( aei );
+                    //Log.d(TAG, "Retrieve Get AEID[" + aei + "]");
+                    in.close();
+                }
+                if (responseCode != 0) {
+                    receiver.getResponseBody( Integer.toString(responseCode) );
+                }
+                conn.disconnect();
+            } catch (Exception exp) {
+                LOG.log(Level.SEVERE, exp.getMessage());
+            }
+        }
     }
 }
