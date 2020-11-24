0b708371f0fefaa0ff5f949e22a20e1119468b05
Bochan kim <44803830+bo1030@users.noreply.github.com>
2020-11-25 08:27:12 +0900

    remove useless code

--- Fake Drone Application Code/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ Fake Drone Application Code/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -23,12 +23,7 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
     TextView battery, positionPitch, positionRoll, positionYaw , vibrationX, vibrationY, vibrationZ, controlPitch, controlRoll, controlYaw, locationX, locationY, locationZ, mod, ekfStatus, task;
 
     private static CSEBase csebase = new CSEBase();
-    private static AE ae = new AE();
-    private static String TAG = "MainActivity";
-    private String MQTTPort = "1883";
     private String ServiceAEName = "CDP5";
-    private String MQTT_Req_Topic = "";
-    private String MQTT_Resp_Topic = "";
     private ParseElementXml par = null;
     private String Mobius_Address ="13.209.165.214";
     public Handler handler;
@@ -71,46 +66,6 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         ekfStatus = (TextView) findViewById(R.id.efkInfo);
         task = (TextView) findViewById(R.id.missionInfo);
         csebase.setInfo(Mobius_Address,"7579","Mobius","1883");
-    }
-
-    public void GetAEInfo() {
-        //csebase.setInfo("203.253.128.151","7579","Mobius","1883");
-        // AE Create for Android AE
-        ae.setAppName("ncubeapp");
-        aeCreateRequest aeCreate = new aeCreateRequest();
-        aeCreate.setReceiver(new IReceived() {
-            public void getResponseBody(final String msg) {
-                handler.post(new Runnable() {
-                    public void run() {
-                        Log.d(TAG, "** AE Create ResponseCode[" + msg +"]");
-                        if( Integer.parseInt(msg) == 201 ){
-                            MQTT_Req_Topic = "/oneM2M/req/Mobius2/"+ae.getAEid()+"_sub"+"/#";
-                            MQTT_Resp_Topic = "/oneM2M/resp/Mobius2/"+ae.getAEid()+"_sub"+"/json";
-                            Log.d(TAG, "ReqTopic["+ MQTT_Req_Topic+"]");
-                            Log.d(TAG, "ResTopic["+ MQTT_Resp_Topic+"]");
-                        }
-                        else { // If AE is Exist , GET AEID
-                            aeRetrieveRequest aeRetrive = new aeRetrieveRequest();
-                            aeRetrive.setReceiver(new IReceived() {
-                                public void getResponseBody(final String resmsg) {
-                                    handler.post(new Runnable() {
-                                        public void run() {
-                                            Log.d(TAG, "** AE Retrive ResponseCode[" + resmsg +"]");
-                                            MQTT_Req_Topic = "/oneM2M/req/Mobius2/"+ae.getAEid()+"_sub"+"/#";
-                                            MQTT_Resp_Topic = "/oneM2M/resp/Mobius2/"+ae.getAEid()+"_sub"+"/json";
-                                            Log.d(TAG, "ReqTopic["+ MQTT_Req_Topic+"]");
-                                            Log.d(TAG, "ResTopic["+ MQTT_Resp_Topic+"]");
-                                        }
-                                    });
-                                }
-                            });
-                            aeRetrive.start();
-                        }
-                    }
-                });
-            }
-        });
-        aeCreate.start();
     }
 
     public void onClick(View v) {
@@ -303,134 +258,6 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
 
             } catch (Exception exp) {
                 LOG.log(Level.WARNING, exp.getMessage());
-            }
-        }
-    }
-
-     class aeCreateRequest extends Thread {
-        private final Logger LOG = Logger.getLogger(aeCreateRequest.class.getName());
-        String TAG = aeCreateRequest.class.getName();
-        private IReceived receiver;
-        int responseCode=0;
-        public ApplicationEntityObject applicationEntity;
-        public void setReceiver(IReceived hanlder) { this.receiver = hanlder; }
-        public aeCreateRequest(){
-            applicationEntity = new ApplicationEntityObject();
-            applicationEntity.setResourceName(ae.getappName());
-        }
-        @Override
-        public void run() {
-            try {
-
-                String sb = csebase.getServiceUrl();
-                URL mUrl = new URL(sb);
-
-                HttpURLConnection conn = (HttpURLConnection) mUrl.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                conn.setUseCaches(false);
-                conn.setInstanceFollowRedirects(false);
-
-                conn.setRequestProperty("Content-Type", "application/vnd.onem2m-res+xml;ty=2");
-                conn.setRequestProperty("Accept", "application/xml");
-                conn.setRequestProperty("locale", "ko");
-                conn.setRequestProperty("X-M2M-Origin", "S"+ae.getappName());
-                conn.setRequestProperty("X-M2M-RI", "12345");
-                conn.setRequestProperty("X-M2M-NM", ae.getappName() );
-
-                String reqXml = applicationEntity.makeXML();
-                conn.setRequestProperty("Content-Length", String.valueOf(reqXml.length()));
-
-                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-                dos.write(reqXml.getBytes());
-                dos.flush();
-                dos.close();
-
-                responseCode = conn.getResponseCode();
-
-                BufferedReader in = null;
-                String aei = "";
-                if (responseCode == 201) {
-                    // Get AEID from Response Data
-                    in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-                    String resp = "";
-                    String strLine;
-                    while ((strLine = in.readLine()) != null) {
-                        resp += strLine;
-                    }
-
-                    ParseElementXml pxml = new ParseElementXml();
-                    aei = pxml.GetElementXml(resp, "aei");
-                    ae.setAEid( aei );
-                    Log.d(TAG, "Create Get AEID[" + aei + "]");
-                    in.close();
-                }
-                if (responseCode != 0) {
-                    receiver.getResponseBody( Integer.toString(responseCode) );
-                }
-                conn.disconnect();
-            } catch (Exception exp) {
-                LOG.log(Level.SEVERE, exp.getMessage());
-            }
-
-        }
-    }
-
-    class aeRetrieveRequest extends Thread {
-        private final Logger LOG = Logger.getLogger(aeCreateRequest.class.getName());
-        private IReceived receiver;
-        int responseCode=0;
-
-        public aeRetrieveRequest() {
-        }
-        public void setReceiver(IReceived hanlder) {
-            this.receiver = hanlder;
-        }
-
-        @Override
-        public void run() {
-            try {
-                String sb = csebase.getServiceUrl()+"/"+ ae.getappName();
-                URL mUrl = new URL(sb);
-
-                HttpURLConnection conn = (HttpURLConnection) mUrl.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                conn.setDoOutput(false);
-
-                conn.setRequestProperty("Accept", "application/xml");
-                conn.setRequestProperty("X-M2M-RI", "12345");
-                conn.setRequestProperty("X-M2M-Origin", "Sandoroid");
-                conn.setRequestProperty("nmtype", "short");
-                conn.connect();
-
-                responseCode = conn.getResponseCode();
-                BufferedReader in = null;
-                String aei = "";
-                if (responseCode == 200) {
-                    // Get AEID from Response Data
-                    in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-                    String resp = "";
-                    String strLine;
-                    while ((strLine = in.readLine()) != null) {
-                        resp += strLine;
-                    }
-
-                    ParseElementXml pxml = new ParseElementXml();
-                    aei = pxml.GetElementXml(resp, "aei");
-                    ae.setAEid( aei );
-                    //Log.d(TAG, "Retrieve Get AEID[" + aei + "]");
-                    in.close();
-                }
-                if (responseCode != 0) {
-                    receiver.getResponseBody( Integer.toString(responseCode) );
-                }
-                conn.disconnect();
-            } catch (Exception exp) {
-                LOG.log(Level.SEVERE, exp.getMessage());
             }
         }
     }
